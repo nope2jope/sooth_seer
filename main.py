5604ee287822675e90586e12c23dcdb7326de247
nope2jope <67401076+nope2jope@users.noreply.github.com>
2021-07-29 10:35:40 -0700

    improved button functionality

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def home():
-    cards = [0, 1, 3]
-    return render_template("index.html", c=cards)
+    cards = 0
+    clicked = False
+    return render_template("index.html", c=cards, bool=clicked)
 
-
+@app.route('/one-card')
+def one_card():
+    cards = 1
+    clicked = True
+    return render_template("index.html", c=cards, bool=clicked)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
